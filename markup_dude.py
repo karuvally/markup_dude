7eba726c84a0b79e640fbeff29d270092dc27c8b
Aswin Karuvally <aswinbabuk@gmail.com>
2021-04-04 12:40:15 +0530

    added args

---
+++ markup_dude.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# Convert Markdown to HTML, made for https://karuvally.github.io
+
+# Import required libraries
+import argparse
+
+
+# The main function
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description = "Convert Markdown to HTML"
+    )
+    parser.add_argument(
+        "INPUT",
+        type = str,
+        help = "Markdown document for conversion"
+    )
+    main()
+
