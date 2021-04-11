2307a3e35ed0b899aa5c2256d4bedece09de8299
Aswin Karuvally <aswinbabuk@gmail.com>
2021-04-11 10:54:30 +0530

    added comments

--- src/markup_dude.go
+++ src/markup_dude.go
@@ -11,9 +11,13 @@ import (
 
 // The main function
 func main() {
+    // Path to file
     var path = "sample.md"
+
+    // Open the file in read mode
     var file, err = os.Open(path)
 
+    // Produce error, if the file cannot be read
     if err != nil {
         log.Fatal(err)
     }
